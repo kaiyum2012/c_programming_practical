e1de75a244fb8e2cdf811649aeba48dd98225b0f
Abdulkaiyum Shaikh <kaiyum2012@gmail.com>
2018-08-16 11:06:09 +0530

    2_2 change in comment and

--- 2_2_result_by_enroll_num.c
+++ 2_2_result_by_enroll_num.c
@@ -3,12 +3,14 @@
 int main(int argc, char const *argv[])
 {
 	int RollNumber;
-	char Name[20];
+	char Name[20]; // sting having 20 char length
+	// char Name;
+
 	float Mark;
 
 	// RollNumber = 34;
 	// Mark = 34.5;
-
+	// Assume that Roll number is Numeric Only
 	printf("Enter Roll number \n");
 	scanf("%d", &RollNumber);
 
@@ -16,20 +18,22 @@ int main(int argc, char const *argv[])
 	// way 1
 	// gets(Name);
 
-	// way 2
+	// way 2	
 	// fgets(Name,20,stdin);
 
 	// way 3
 	scanf("%s", Name);
 	
+	// scanf("%c",&Name);
+
 	getchar();
 
 	printf("Enter Mark \n");
 	scanf("%f", &Mark);
 	getchar();
 
-	printf("Roll Number \t Name \t Mark \n");
+	printf("Roll Number\tName\tMark \n");
 	printf("%d \t %s \t %f \n",RollNumber,Name,Mark);
 
-	return 0;
+	return 0; // getch();
 }
