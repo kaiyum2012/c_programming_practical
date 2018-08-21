a43d97ee4793430f8c26492b19ea11005e568bde
Abdulkaiyum Shaikh <kaiyum2012@gmail.com>
2018-08-21 11:02:37 +0530

    3_1 Quotient and divisor ( Expression and operator)

---
+++ 3_1_quotient_divisor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int divisor,dividend;
+	float product,remainder,quotient;
+
+
+	printf("Enter dividend\n");
+	scanf("%d", &dividend);
+
+	printf("Enter divisor\n");
+	scanf("%d",&divisor);
+
+
+	// TODO:: Question
+	// 
+	// 1. Find Product = * Done 
+	
+	product = divisor * dividend;
+	printf("Product of %d and %d is %.3f\n", dividend,divisor,product);
+	
+	// 2. Find Quotient = / 
+	quotient = dividend / divisor;	
+	printf("Quotient for dividend = %d and divisor = %d is %f\n",dividend,divisor,quotient);
+
+	// 3. Find Remainder = %
+	remainder = dividend % divisor;
+	printf("Remainder for dividend = %d and divisor = %d is %f\n",dividend,divisor,remainder);
+
+	return 0;
+}
